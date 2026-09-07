b1dddc70a4a2c23715a0806657d45f33eccde58a
Justsomerandomcoder-vibecode <frosticefrostice@gmail.com>
2026-09-07 19:11:36 +0800

    Add config file

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+const config = {
+  IDLE_TIMEOUT: 5 * 60 * 1000, // 5 minutes in milliseconds
+  MAX_QUEUE_DISPLAY: 10, // Tracks shown per queue page
+  DEFAULT_VOLUME: 50, // 0-100
+  SEARCH_RESULTS: 5, // Number of search results to show
+};
+
+module.exports = config;
